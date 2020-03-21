bc5a399525ed9161bd5967d42429cac10459f34d
Ilyas K <32903511+rt-fan@users.noreply.github.com>
2020-03-21 16:11:27 +0300

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,7 @@
+token = '969239675:AAEgA9zEbw0dV9akznYLiau2A_boSftElFI'
+
+user = "support"
+psw = "rtfan35a"
+
+urlCS = 'http://10.11.0.2:8080/bgbilling/executer/json/ru.bitel.bgbilling.kernel.contract.api/ContractService'
+urlBalance = 'http://10.11.0.2:8080/bgbilling/executer/json/ru.bitel.bgbilling.kernel.contract.balance/BalanceService'
